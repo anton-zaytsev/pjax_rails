db577d262a0ecba97540032956c581170110a283
Anton <antonzaycev55@gmail.com>
2011-08-20 14:52:27 +0700

    fixed for own needs on project, changed pjax source to fork of ckald

--- lib/assets/javascripts/pjax/enable_pjax.js.coffee
+++ lib/assets/javascripts/pjax/enable_pjax.js.coffee
@@ -1,1 +1,1 @@
-$ -> $('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])').pjax('[data-pjax-container]')
+$ -> $('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax]):not([href^="http"]):not([href^="#"]):not([target="_blank"])').pjax('[data-pjax-container]')
--- lib/pjax.rb
+++ lib/pjax.rb
@@ -1,30 +1,29 @@
 module Pjax
   extend ActiveSupport::Concern
-  
+
   included do
-    layout ->(c) { pjax_request? ? false : 'application' }
-    helper_method :pjax_request?
+    layout ->(c) { pjax_request? ? 'application-pjax' : 'application' }
   end
-  
-  private  
-    def redirect_pjax_to(action, url = nil)
-      new_url = url_for(url ? url : { action: action })
-      
-      render js: <<-EJS
+
+  private
+  def redirect_pjax_to(action, url = nil)
+    new_url = url_for(url ? url : { action: action })
+
+    render js: <<-EJS
         if (!window.history || !window.history.pushState) {
-          window.location.href = '#{new_url}';
+          window.location.href = '#{render_to_string("#{action}.html.erb").to_json}';
         } else {
-          $('[data-pjax-container]').html(#{render_to_string("#{action}.html.erb", layout: false).to_json});
+          $('[data-pjax-container]').html('');
           $(document).trigger('end.pjax');
 
           var title = $.trim($('[data-pjax-container]').find('title').remove().text());
           if (title) document.title = title;
           window.history.pushState({}, document.title, '#{new_url}');
         }
-      EJS
-    end
+    EJS
+  end
 
-    def pjax_request?
-      env['HTTP_X_PJAX'].present?
-    end
+  def pjax_request?
+    env['HTTP_X_PJAX'].present?
+  end
 end
